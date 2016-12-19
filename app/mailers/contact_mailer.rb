bf787cd95b13173728594fe6d47c8b5b6985f700
Piotr Lasota <lasota.piotr1@gmail.com>
2016-12-19 15:22:34 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMariler::Base
+  default to: 'lso@30wave.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
